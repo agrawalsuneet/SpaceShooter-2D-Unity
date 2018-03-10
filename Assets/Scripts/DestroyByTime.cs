feb416967c3a66c695169b965b5f6a6ac41dcfc4
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-03-10 18:50:57 +0000

    added asteroid wave

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+
+	public float lifetime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, lifetime);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,18 +7,33 @@ public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValues;
 
+	public int hazardCount = 10;
+	public float spawnWait = 0.5f;
+	public float startWait = 2.0f;
+	public float waveWait = 3.0f;
+
 	// Use this for initialization
 	void Start () {
-		spawnHazards ();
+		StartCoroutine (spawnHazards());
 	}
 	
-	void spawnHazards(){
-		Vector3 spawnPosition = new Vector3 (
-			Random.Range(-spawnValues.x, spawnValues.x),
-			spawnValues.y,
-			spawnValues.z);
+	IEnumerator spawnHazards(){
+		yield return new WaitForSeconds (startWait);
 
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (
+					                       Random.Range (-spawnValues.x, spawnValues.x),
+					                       spawnValues.y,
+					                       spawnValues.z);
+
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds (spawnWait);
+			}
+
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
 }
